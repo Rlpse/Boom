1cbc7f92b9f690d6da6e171bcda8e588bb800c14
Ant <r3dzon3r@gmail.com>
2020-06-02 19:55:42 +0100

    Tatusia kupka.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -19,13 +19,40 @@ public class PlayerMovement : MonoBehaviour {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
+    public static Vector2 Rotate(Vector2 v, float degrees)
+    {
+        float sin = Mathf.Sin(degrees * Mathf.Deg2Rad);
+        float cos = Mathf.Cos(degrees * Mathf.Deg2Rad);
+
+        float tx = v.x;
+        float ty = v.y;
+        v.x = (cos * tx) - (sin * ty);
+        v.y = (sin * tx) + (cos * ty);
+        return v;
+    }
+
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        //rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
 
         Vector2 lookDir = mousePos - rb.position;
+        if (Input.GetKey("w"))
+        {
+            rb.MovePosition(rb.position + lookDir * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.MovePosition(rb.position + -lookDir * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.MovePosition(rb.position + Rotate(lookDir, 90.0f) * moveSpeed * Time.fixedDeltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.MovePosition(rb.position + Rotate(lookDir, -90.0f) * moveSpeed * Time.fixedDeltaTime);
+        }
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
-
         rb.rotation = angle;
     }
 
